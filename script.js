6eda0f355dfb847fc642973eaf6fc34c875865a9
davide-delgiudice <dav.del.giudice@gmail.com>
2025-03-17 16:18:34 +0100

    [UPD] add console log with final price

--- script.js
+++ script.js
@@ -4,22 +4,24 @@
 // - va applicato uno sconto del 40% per gli over 65.
 // - L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
 
-let utenteKm = parseInt(prompt('Inserisci il numero di km che desideri percorrere'));
+let utenteKm = prompt('Inserisci il numero di km che desideri percorrere');
 
-let utenteEta = parseInt(prompt('Inserisci la tua età'));
+let utenteEta = prompt('Inserisci la tua età');
 
 let biglietto = 0.21 * utenteKm;
 
 let prezzoFinale;
 
-if(utenteEta < 18){
-    prezzoFinale = biglietto - biglietto * 0.20;
+if(utenteEta <= 17){
+    prezzoFinale = biglietto - biglietto * 0.2;
 }
 
 else if(utenteEta >= 65){
-    prezzoFinale = biglietto - biglietto *0.40;
+    prezzoFinale = biglietto - biglietto *0.4;
 }
 
 else{
     prezzoFinale = biglietto;
 }
+
+console.log(`Il prezzo finale da pagare è di ${prezzoFinale}`);
